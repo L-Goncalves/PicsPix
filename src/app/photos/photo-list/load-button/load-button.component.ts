aa8eade5584573102961555d21a4c253c85732e3
l-goncalves <ldgoncalvs@gmail.com>
2021-01-05 17:51:16 -0300

    Changes Done

--- src/app/photos/photo-list/load-button/load-button.component.ts
+++ src/app/photos/photo-list/load-button/load-button.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LoadButtonComponent implements OnInit {
 
-  @Input() hasMoe: boolean = false;
+  @Input() hasMore: boolean = false;
 
   constructor() { }
 
---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,32 @@
+import { SearchComponent } from './search/search.component';
+import { CardModule } from './../../shared/components/card/card.module';
+import { CommonModule } from '@angular/common';
+import { FilterByDescription } from './filter-by-description.pipe';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { PhotosComponent } from './photos/photos.component';
+import { PhotoListComponent } from './photo-list.component';
+import { NgModule } from '@angular/core';
+import { PhotoModule } from '../photo/photo.module';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
+
+
+
+@NgModule({
+    declarations: [
+        PhotoListComponent,
+        PhotosComponent,
+        LoadButtonComponent,
+        FilterByDescription,
+        SearchComponent
+    ],
+    imports: [
+        CommonModule,
+        PhotoModule,
+        CardModule,
+        DarkenOnHoverModule
+    ]
+})
+
+export class PhotoListModule{
+
+}
