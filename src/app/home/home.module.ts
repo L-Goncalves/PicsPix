cf335d568f018659ddaf69d914bcdcb864895bd5
l-goncalves <ldgoncalvs@gmail.com>
2021-01-06 10:05:34 -0300

    Commiting new changes

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { VMessageModule } from './../shared/components/vmessage/vmessage.module';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SignInComponent } from './signIn/signIn.component';
+import { ReactiveFormsModule } from "@angular/forms";
+@NgModule({
+    declarations: [SignInComponent],
+    imports: [
+        ReactiveFormsModule,
+        CommonModule,
+        VMessageModule
+    ]
+})
+
+export class HomeModule{
+
+}
---
+++ src/app/shared/components/vmessage/vmessage.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-vmessage',
+    templateUrl: './vmessage.component.html'
+
+})
+
+
+export class VMessageComponent{
+
+   @Input() text = '';
+}
+
