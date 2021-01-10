073f4fc172911231806cca86a7d5a87c273a413d
l-goncalves <ldgoncalvs@gmail.com>
2021-01-10 18:54:06 -0300

    Creating Header Component

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { TokenService } from './../token/token.service';
+import { UserService } from './../user/user.service';
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {tap } from 'rxjs/operators'
@@ -11,14 +12,14 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private tokenService: TokenService) {}
+    private userService: UserService) {}
 
   authenticate(userName: string, password: string){
     return this.http
       .post(API_URL + '/user/login', {userName, password}, {observe: 'response'})
       .pipe(tap(res =>{
         const authToken = res.headers.get('x-access-token');
-        this.tokenService.setToken(authToken)
+        this.userService.setToken(authToken)
  
       }))
   }
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,10 @@
+import { NgModule, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+
+export class HeaderComponent{
+
+}
